7130390ed79853bd833d90240efed3cf9916fb7b
Chris <yyin@ualberta.ca>
2019-01-05 15:10:23 -0700

    correct error in key.js file

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 //key.js - figure out what set of credential to return
-if (process.evn.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production'){
   // we are in production - return the prod set of keys
   modeule.export = require('./prod');
 }else{
